455cdfcaea83b783f8b606fbd42b2ed026c5619f
huangxinhere <1159390360@qq.com>
2020-12-08 15:31:38 +0800

    第四章的挑战

--- QuizeActivity/app/src/main/java/com/example/quizeactivity/Question.java
+++ QuizeActivity/app/src/main/java/com/example/quizeactivity/Question.java
@@ -3,6 +3,7 @@ package com.example.quizeactivity;
 public class Question {
     private int mTestResId;
     private boolean mAnswerTrue;
+    private boolean mAnswered;//用来标记是否完成回答，新增了一个实例
 
     public boolean isAnswerTrue() {
         return mAnswerTrue;//和下面方法的区别set；set：接受值赋予给实例//is就是返回本身
@@ -20,8 +21,17 @@ public class Question {
         mTestResId = testResId;
     }
 
+    public void setAnswered(boolean answered){//问题：answered从哪里来？？
+        mAnswered = answered;
+    }
+
+    public boolean isAnswered(){
+        return mAnswered;
+    }
+
     public Question(int textResId, boolean answerTrue){
         mTestResId=textResId;
         mAnswerTrue=answerTrue;
     }
+
 }
