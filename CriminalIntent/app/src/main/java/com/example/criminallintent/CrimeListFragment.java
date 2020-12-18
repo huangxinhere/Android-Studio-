4c6e86f64e4c0f7d9c9c6268c34292fc3dc854a3
huangxinhere <1159390360@qq.com>
2020-12-18 17:45:51 +0800

    the second project

--- CriminalIntent/app/src/main/java/com/example/criminallintent/CrimeListFragment.java
+++ CriminalIntent/app/src/main/java/com/example/criminallintent/CrimeListFragment.java
@@ -5,12 +5,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class CrimeListFragment extends Fragment {
     private RecyclerView mCrimeRecyclerView;
+    private CrimeAdapter mAdapter;
 
     @Override/*使用布局并找到布局中的RecyclerView视图*/
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -23,12 +27,52 @@ public class CrimeListFragment extends Fragment {
         mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         /*没有LayoutManager的支持，不仅RecyclerView无法工作，还会导致应用崩溃*/
 
+        updateUI();
+
         return view;//返回给托管的activity？
     }
 
-    private class CrimeHolder extends Fragment{
+    /*关联Adapter和RecyclerView：创建CrimeAdapter，设置给RecyclerView*/
+    private void updateUI(){
+        CrimeLab crimeLab = CrimeLab.get(getActivity());//怎么分析？？
+        List<Crime> crimes = crimeLab.getCrimes();
+
+        mAdapter = new CrimeAdapter(crimes);
+        mCrimeRecyclerView.setAdapter(mAdapter);
+    }
+
+    /*定义ViewHolder内部类：实例化并使用list_item_crime布局*/
+    private class CrimeHolder extends RecyclerView.ViewHolder{
         public CrimeHolder(LayoutInflater inflater,ViewGroup parent){
             super(inflater.inflate(R.layout.list_item_crime,parent,false));
+        }
+    }
+
+    /*Adapter:显示新创建的ViewHolder或让Crime对象和已创建的ViewHolder关联*/
+    private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder>{
+
+        private List<Crime> mCrimes;
+
+        public CrimeAdapter(List<Crime> crimes){
+            mCrimes = crimes;
+        }
+
+        @NonNull
+        @Override
+        public CrimeHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+           LayoutInflater layoutInflater = LayoutInflater.from(getActivity());//菜鸟教程里有
+
+            return new CrimeHolder(layoutInflater,parent);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull CrimeHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
         }
     }
 }
