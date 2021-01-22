d941aea2365b2de906bc5fc5ba9e103117cd19e9
huangxinhere <1159390360@qq.com>
2021-01-22 20:26:32 +0800

    十

--- CriminalIntent/app/src/main/java/com/example/criminallintent/CrimePagerActivity.java
+++ CriminalIntent/app/src/main/java/com/example/criminallintent/CrimePagerActivity.java
@@ -1,6 +1,8 @@
 package com.example.criminallintent;
 
 import android.app.AppComponentFactory;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,31 +13,54 @@ import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import java.util.List;
+import java.util.UUID;
 
 public class CrimePagerActivity extends AppCompatActivity {
 
+    private static final String EXTRA_CRIME_ID =
+            "com.example.criminalIntent.crime_id";
     private ViewPager mViewPager;
     private List<Crime> mCrimes;
+
+    public static Intent newIntent(Context packageContext, UUID crimeId){
+        Intent intent = new Intent(packageContext,CrimePagerActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID,crimeId);
+        return intent;
+    }
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime_pager);
 
-        mViewPager = (ViewPager) findViewById(R.id.crime_view_pager);
+        UUID crimeId = (UUID) getIntent()
+                .getSerializableExtra(EXTRA_CRIME_ID);
+
+        mViewPager = (ViewPager) findViewById(R.id.crime_recycle_view);
 
         mCrimes = CrimeLab.get(this).getCrimes();
         FragmentManager fragmentManager = getSupportFragmentManager();
         mViewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
-            @Override
-            public int getCount() {
-                return 0;
-            }
 
-            @NonNull
+            /*获取并显示crime数组中指定位置的Crime时，它会返回配置
+              过的CrimeFragment   来显示指定的Crime*/
             @Override
             public Fragment getItem(int position) {
-                return null;
+                Crime crime = mCrimes.get(position);
+                return CrimeFragment.newInstance(crime.getId());
+            }
+
+            @Override
+            public int getCount() {
+                return mCrimes.size();
             }
         });
+
+        //设置初始分页显示项
+        for (int i = 0; i < mCrimes.size(); i++){
+            if (mCrimes.get(i).getId().equals(crimeId)){
+                mViewPager.setCurrentItem(i);
+                break;
+            }
+        }
     }
 }
